90aa22d6715b0b3e42b74bf2d06b210fb319341b
andzow <andreywsaers@gmail.com>
2024-11-10 14:31:22 +0300

    asdasd

--- server/utils/telegram/sendApplication.js
+++ server/utils/telegram/sendApplication.js
@@ -3,6 +3,7 @@ import TelegramBot from 'node-telegram-bot-api';
 export const sendApplication = async (chatId, message) => {
     try {
         const token = process.env.TELEGRAM_TOKEN;
+        console.log(token)
         const bot = new TelegramBot(token, {polling: true});
         await bot.sendMessage(chatId, message);
     } catch (error) {
